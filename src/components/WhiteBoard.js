50ea2da43a2a43fc69aba2a1865cb310f0e5813c
thomasadohle <dohle.t@husky.neu.edu>
2019-02-01 13:48:41 -0500

    finally got the toggle to work!

--- src/components/WhiteBoard.js
+++ src/components/WhiteBoard.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react'
-import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Link, Route, Redirect} from 'react-router-dom'
 import CourseGrid from './CourseGrid'
 import CourseTable from './CourseTable'
 import CourseService from '../services/CourseService'
 import CourseEditor from "./CourseEditor";
 import "./Styling/course-editor.style.client.css"
 import "./Styling/course-list.style.client.css"
+var grid = "grid"
+var table = "table"
 class WhiteBoard extends Component {
     constructor() {
         super();
@@ -13,32 +15,70 @@ class WhiteBoard extends Component {
         var modules = this.courseService.findCourseModules(123)
         var lessons = this.courseService.findLessons(modules)
         var topics = this.courseService.findTopics(lessons)
+
         this.state = {
-            courses: this.courseService.findAllCourses()
+            courses: this.courseService.findAllCourses(),
+            courseView: grid
         }
     }
     deleteCourse = course =>
         this.setState({
-            courses: this.courseService.deleteCourse(course)
+            courses: this.courseService.deleteCourse(course),
         })
     addCourse = () =>
         this.setState({
             courses: this.courseService.addCourse(null)
         })
+
+    toggleViews = () => {
+
+        if (this.state.courseView === grid) {
+            console.log("Before event: " + this.state.courseView)
+            this.setState({
+                courseView: table
+            })
+            console.log("After event: " + this.state.courseView)
+            return "/table"
+        } else {
+            console.log("Before event: " + this.state.courseView)
+            this.setState({
+                courseView: grid
+            })
+            console.log("After event: " + this.state.courseView)
+            return "/"
+        } 
+    }
+
+
     render() {
         return (
             <div>
                 <Router>
                     <div>
-                        <Link to="/">Course Grid</Link> |
+                        <Link to={this.toggleViews}>Course Grid</Link> |
                         <Link to="/table">Course Table</Link>
+                        {this.state.courseView ===grid &&
+                        <Link to="/table">
+                            <button className="btn btn-primary"
+                                    onClick={this.toggleViews}>
+                                Toggle</button>
+                        </Link>
+                        }
+                        {this.state.courseView ===table &&
+                        <Link to="/">
+                            <button className="btn btn-primary"
+                                    onClick={this.toggleViews}>
+                                Toggle</button>
+                        </Link>
+                        }
+
                         <Route path='/' exact
                                render={() =>
                                    <CourseGrid
                                        addCourse={this.addCourse}
                                        deleteCourse={this.deleteCourse}
                                        courses={this.state.courses}
-                                        courseService={this.courseService}/>}/>
+                                       courseService={this.courseService}/>}/>
                         <Route path="/course/:id"
                                exact
                                component={CourseEditor}/>
