ea81cbe4a48af83e48699578bd71bdcc3c6a480b
thomasadohle <dohle.t@husky.neu.edu>
2019-02-07 14:20:50 -0500

    image widget renders correctly

--- src/components/WidComponent.js
+++ src/components/WidComponent.js
@@ -1,26 +1,62 @@
 import React from 'react'
 import WidHeading from './WidHeading'
 import WidImage from './WidImage'
+//
+// const WidComponent = ({widget, deleteWidget, updateWidget}) =>
+//     <div>
+//         <button onClick={() => deleteWidget(widget)}>Delete</button>
+//         <select
+//             onChange={(event) => {
+//                 widget.type = event.target.value
+//                 updateWidget(widget)
+//             }}
+//             className="form-control" value={widget.type}>
+//             <option value="HEADING">Heading</option>
+//             <option value="IMAGE">Image</option>
+//         </select>
+//         {
+//             widget.type=='HEADING' &&
+//             <Widget-Heading
+//                 updateWidget={updateWidget}
+//                 widget={widget}/> ||
+//             widget.type=='IMAGE'   && <WidImage   widget={widget}/>
+//         }
+//     </div>
+
+
+
 
 const WidComponent = ({widget, deleteWidget, updateWidget}) =>
-    <div>
-        <button onClick={() => deleteWidget(widget)}>Delete</button>
-        <select
-            onChange={(event) => {
-                widget.type = event.target.value
-                updateWidget(widget)
-            }}
-            className="form-control" value={widget.type}>
-            <option value="HEADING">Heading</option>
-            <option value="IMAGE">Image</option>
-        </select>
-        {
-            widget.type=='HEADING' &&
-            <Widget-Heading
-                updateWidget={updateWidget}
-                widget={widget}/> ||
-            widget.type=='IMAGE'   && <WidImage   widget={widget}/>
-        }
-    </div>
+    <div className="row col-10 container wbdv-heading-widget centered wbdv-widget"
+         data-name="sample-image-widget">
+        <div className="row col-lg-12 wbdv-widget-first-row">
+            <div className="col-lg-6 wbdv-widget-title">
+                <h3>{widget.title}</h3>
+            </div>
+            <div className="col-lg-1 ">
+                <ion-icon className="wbdv-widget-move-icon" name="arrow-round-up" size="large"></ion-icon>
+            </div>
+            <div className="col-lg-1 ">
+                <ion-icon className="wbdv-widget-move-icon" name="arrow-round-down" size="large"></ion-icon>
+            </div>
+            <div className="col-lg-3 wbdv-widget-select-widget-type">
+                <select className="custom-select custom-select-lg mb-3">
+                    <option selected>{widget.type}</option>
+                    <option value="1">Heading</option>
+                    <option value="2">List</option>
+                    <option value="3">Image</option>
+                    <option value="4">Link</option>
+                </select>
+            </div>
+            <div className="col-lg-1 ">
+                <ion-icon className="wbdv-widget-delete-icon" name="close-circle-outline"
+                          size="large"></ion-icon>
+            </div>
 
+        </div>
+        {widget.type ==="IMAGE" &&
+        <WidImage widget={widget}
+                  updateWidet={updateWidget}/>}
+
+    </div>
 export default WidComponent
--- src/components/WidImage.js
+++ src/components/WidImage.js
@@ -2,56 +2,29 @@ import React from 'react'
 import "./Styling/course-editor.style.client.css"
 
 
+const WidImage = ({widget, updateWidget}) =>
+    <div className="row col-lg-12 wbdv-heading-widget-enter-heading">
 
-export default class WidImage
-    extends React.Component {
-
-    render() {
-        return(
-            <div className="row col-10 container wbdv-heading-widget centered wbdv-widget"
-                 data-name="sample-image-widget">
-                <div className="row col-lg-12 wbdv-widget-first-row">
-                    <div className="col-lg-6 wbdv-widget-title">
-                        <h3>Image Widget</h3>
-                    </div>
-                    <div className="col-lg-1 ">
-                        <ion-icon className="wbdv-widget-move-icon" name="arrow-round-up" size="large"></ion-icon>
-                    </div>
-                    <div className="col-lg-1 ">
-                        <ion-icon className="wbdv-widget-move-icon" name="arrow-round-down" size="large"></ion-icon>
-                    </div>
-                    <div className="col-lg-3 wbdv-widget-select-widget-type">
-                        <select className="custom-select custom-select-lg mb-3">
-                            <option selected>Image</option>
-                            <option value="1">Heading</option>
-                            <option value="2">List</option>
-                            <option value="3">Image</option>
-                            <option value="4">Link</option>
-                        </select>
-                    </div>
-                    <div className="col-lg-1 ">
-                        <ion-icon className="wbdv-widget-delete-icon" name="close-circle-outline"
-                                  size="large"></ion-icon>
-                    </div>
-                </div>
-                <div className="row col-lg-12 wbdv-heading-widget-enter-heading">
-                    <div className="form-group col-12">
-                        <input className="form-control" placeholder="https://ksassets.timeincuk.net/wp/uploads/sites/55/2017/08/2017_GameOfThrones_HBO_220817.jpg"
-                               type="text"></input>
-                    </div>
-                </div>
-                <div className="row col-lg-12 wbdv-heading-widget-enter-name">
-                    <div className="form-group col-12">
-                        <input className="form-control" placeholder="Widget name" type="text"></input>
-                    </div>
-                </div>
-                <div className="row col-lg-12">
-                    <h3>Preview</h3>
-                </div>
-                <div className="row col-lg-12">
-                    <img
-                        className="wbdv-image-widget-image responsive"
-                        src='https://ksassets.timeincuk.net/wp/uploads/sites/55/2017/08/2017_GameOfThrones_HBO_220817.jpg'></img>
-                </div>
-            </div>
-        );}}
+        <div className="form-group row col-12">
+            <label className="col-2 col-form-label">Image URL</label>
+            <input className="form-control col-10"
+                   placeholder={widget.image.url}
+                   type="text"></input>
+        </div>
+        <div className="form-group row col-12">
+            <label className="col-2 col-form-label">Image URL</label>
+            <input className="form-control col-10" placeholder="Widget name" type="text"></input>
+        </div>
+        <button onClick={() => updateWidget(widget)}
+                className="btn btn-primary">Update
+        </button>
+        <div className="row col-lg-12">
+            <h3>Preview</h3>
+        </div>
+        <div className="row col-lg-12">
+            <img
+                className="wbdv-image-widget-image responsive"
+                src={widget.image.url}></img>
+        </div>
+    </div>
+export default WidImage
