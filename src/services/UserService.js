dbe915f8916c11b7c9a20b7ee17b51c4db519121
thomasadohle <dohle.t@husky.neu.edu>
2019-03-29 08:52:07 -0400

    trying to fix CORS issue

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -5,63 +5,64 @@ class _UserService {
 
 
     registerUser = (user) => {
-        const url = this.baseUrl + "/api/register"
-        console.log("user received by registerUser in UserService is: " + JSON.stringify(user))
-        console.log("url is: " + url)
+        const url = this.baseUrl + "/api/register";
+        console.log("user received by registerUser in UserService is: " + JSON.stringify(user));
+        console.log("url is: " + url);
         return fetch(url, {
                 method: "POST",
+                credentials: 'include',
                 body: JSON.stringify(user),
                 headers: {
                     "Content-Type": "application/json",
-                },
-                credentials: 'include'
+                }
+
             }
         )
             .then(response => response.json())
             .then(json => console.log(json)).catch(function (error) {
                 console.log("error in registerUser in UserService: " + error)
             })
-    }
+    };
 
     loginUser = (user) => {
-        const url = this.baseUrl + "/api/login"
-        let loggedInUser
-        console.log("logging in userService with user:" + JSON.stringify(user) + "and url: " + url)
+        const url = this.baseUrl + "/api/login";
+        let loggedInUser;
+        console.log("logging in userService with user:" + JSON.stringify(user) + "and url: " + url);
         return fetch(url,
             {
                 method: "POST",
+                credentials: 'include',
                 body: JSON.stringify(user),
                 headers: {
                     "Content-Type": "application/json"
-                },
-                credentials: 'include',
+                }
             }
         )
             .then(response => {
-                console.log("Fetched loggedInUser: " + response)
+                console.log("Fetched loggedInUser: " + response);
                 return response.json()
             }).catch(error => console.log("error in loginUser: " + error))
-    }
+    };
 
     logoutUser = () => {
-        let url = this.baseUrl + "/api/logout"
+        let url = this.baseUrl + "/api/logout";
         return fetch(url, {
             method: "POST",
             credentials: "include"
         })
-    }
+    };
 
 
     currentUserQuery = () => {
-        const url = this.baseUrl + "/api/profile"
+        const url = this.baseUrl + "/api/profile";
         return fetch(url, {
             method: "POST",
             credentials: 'include'
         }).then(function (response) {
-            console.log("response is: " + response)
+            console.log("response is: " + response);
             return response.json()
         }).then(function (json) {
-            console.log("json is: " + JSON.stringify(json))
+            console.log("json is: " + JSON.stringify(json));
             return json
         })
             .catch(function (error) {
@@ -72,5 +73,5 @@ class _UserService {
 
 }
 
-let userService = new _UserService()
+let userService = new _UserService();
 export default userService
