79f4d642fd13450a1ae204422c379dcf08ea40c0
thomasadohle <dohle.t@husky.neu.edu>
2019-03-30 09:43:42 -0400

    trying to fix CORS issue

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -13,7 +13,7 @@ class _UserService {
                 credentials: 'include',
                 body: JSON.stringify(user),
                 headers: {
-                    "Content-Type": "application/json",
+                    "Content-Type": "text/plain",
                 }
 
             }
